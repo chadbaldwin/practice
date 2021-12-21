b985f23c4a76f456898e8b10e70e9a126efdb124
Chad Baldwin <chad@chadbaldwin.net>
2021-12-20 22:22:34 -0800

    Advent of Code - Day 12 - wip

---
+++ Advent of Code/2021/SQL/Day 12.sql
@@ -0,0 +1,167 @@
+------------------------------------------------------------------------------
+-- Input data
+------------------------------------------------------------------------------
+	IF OBJECT_ID('tempdb..#rawdata','U') IS NOT NULL DROP TABLE #rawdata; --SELECT * FROM #rawdata
+	CREATE TABLE #rawdata (ID int IDENTITY, Val varchar(20) NOT NULL);
+
+	DECLARE @UseSampleData bit = 1;
+
+	IF (@UseSampleData = 1)
+	BEGIN;
+		RAISERROR('Sample data',0,1) WITH NOWAIT;
+		-- 10 paths
+		INSERT INTO #rawdata (Val) VALUES ('start-A'),('start-b'),('A-c'),('A-b'),('b-d'),('A-end'),('b-end')
+
+		-- 19 paths
+		--INSERT INTO #rawdata (Val) VALUES ('dc-end'),('HN-start'),('start-kj'),('dc-start'),('dc-HN'),('LN-dc'),('HN-end'),('kj-sa'),('kj-HN'),('kj-dc')
+
+		-- 226 paths
+		--INSERT INTO #rawdata (Val) VALUES ('fs-end'),('he-DX'),('fs-he'),('start-DX'),('pj-DX'),('end-zg'),('zg-sl'),('zg-pj'),('pj-he'),('RW-he'),('fs-DX'),('pj-RW'),('zg-RW'),('start-pj'),('he-WI'),('zg-he'),('pj-fs'),('start-RW')
+	END;
+	ELSE
+	BEGIN;
+		RAISERROR('Real data',0,1) WITH NOWAIT;
+		INSERT INTO #rawdata (Val) VALUES ('um-end'),('pk-um'),('FE-il'),('ay-FE'),('pk-start'),('end-jt'),('um-FE'),('RO-il'),('xc-ay'),('il-end'),('start-EZ'),('pk-FE'),('xc-start'),('jt-FE'),('EZ-um'),('pk-xc'),('xc-EZ'),('pk-ay'),('il-ay'),('jt-EZ'),('jt-om'),('pk-EZ')
+	END;
+
+	IF OBJECT_ID('tempdb..#data','U') IS NOT NULL DROP TABLE #data; --SELECT * FROM #data
+	CREATE TABLE #data (
+		ID			int			NOT NULL IDENTITY,
+		PathStart	varchar(5) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+		PathEnd		varchar(5) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+	);
+
+	INSERT INTO #data (PathStart, PathEnd)
+	SELECT PathStart = LEFT(r.Val, CHARINDEX('-', r.Val)-1) 
+		, PathEnd = SUBSTRING(r.Val, CHARINDEX('-', r.Val)+1, 20) 
+	FROM #rawdata r
+
+	-- Add reverse paths as well
+	INSERT INTO #data (PathStart, PathEnd)
+	SELECT d.PathEnd, d.PathStart
+	FROM #data d
+
+	-- Remove bad paths
+	DELETE d
+	FROM #data d
+	WHERE d.PathEnd = 'start' OR d.PathStart = 'end'
+
+	/*
+		Should I remove paths that cannot be taken? Or can those be handled later with rules?
+		For example, `a-b`. This connection cannot be used if it is the only path to `b` since it would
+		require that you visit `a` twice.
+		
+		However, if other paths exist, then it is ok. For the sample this is easy to find.
+
+		But what if we have `a-b-C-d`. Then it would appear to be ok. However, that path could be a dead
+		end that ends at `d`, it now needs to walk back, which is an illegal move.
+
+		I think the best course of action here is to rely on rules and elimination. A path that walks
+		back on a small cave is an illegal path, and ends there. At the end, any paths that do not have
+		an `end` are removed, leaving only complete paths.
+	*/
+	------------------------------------------------------------------------------
+	
+	------------------------------------------------------------------------------
+	/*
+		Scratch notes / brainstorming:
+		One way might be able to do it is with building and parsing strings
+		For example...
+
+		Insert the seed records along with their last value:
+		'start-A', 'A'
+		'start-b', 'b'
+
+		Find all paths whose last value <> 'end'
+
+		Join all links whose Start matches the last value to replace the old list:
+		'start-A-c'		, 'c'
+		'start-A-b'		, 'b'
+		'start-A-end'	, 'end'
+		'start-b-A'		, 'A'
+		'start-b-d'		, 'd'
+		'start-b-end'	, 'end'
+
+		We should now have 6 rows with legal paths. Here, delete any previous paths that don't have an 'end' and insert all 6 rows.
+
+		Rinse repeat...Find all paths whose last value <> 'end'. Join all links whose Start matches the last value to replace the old list:
+		'start-A-c'		, 'c' -> 'A'
+		'start-A-b'		, 'b' -> 'A','d','end'
+		'start-b-A'		, 'A' -> 'b','c','end'
+		'start-b-d'		, 'd' -> 'b'
+
+		We now have 8 new paths to add. So we delete all previous paths that don't have an end, and insert these 8 paths.
+		
+		However, we now have a problem...a small cave has appeared twice (`start-b-d-b`)...how do we check for this at each round to identify illegal paths?
+
+		It seems like this can be a simple check, just search the path for the current PathEnd, if it matches, then we skip that path,
+		but we only perform the check for large caves.
+		
+		So maybe something like `IsLegal = IIF(x.Path LIKE '-' + x.PathEnd + '-' AND x.PathEnd = LOWER(x.PathEnd), 0, 1)`
+
+		Here we can (update, ignore, delete?) illegal paths and move on to the next round.
+
+		Rince repeat until there are no more legal paths that do not have an end.
+	*/
+
+	/*
+		Implementation:
+
+		Based on the proposed solution above, it might be possible to do using a recursive CTE, but illegal paths might make that difficult.
+
+		Going to use a loop instead, which should be straightforward to implement from the solution above.
+	*/
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Part 1
+------------------------------------------------------------------------------
+	IF OBJECT_ID('tempdb..#paths','U') IS NOT NULL DROP TABLE #paths; --SELECT * FROM #paths
+	CREATE TABLE #paths (
+		FullPath varchar(100) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+		LastCave varchar(10) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+	)
+	
+	-- Seed records
+	INSERT INTO #paths (FullPath, LastCave)
+	SELECT CONCAT(d.PathStart,',',d.PathEnd), d.PathEnd
+	FROM #data d
+	WHERE d.PathStart = 'start'
+
+	WHILE (1=1)
+	BEGIN;
+		/* I generally hate using MERGE because of all the issues with it. But in this particular scenario, it saves a few lines
+		   Using merge here allows me to perform an INSERT and a DELETE at the same time. Without the use of MERGE, you'd need to
+		   do it in a much more work around way */
+		MERGE INTO #paths o
+		USING (
+			SELECT FullPath = CONCAT(p.FullPath,',',d.PathEnd), LastCave = d.PathEnd
+			FROM #paths p
+				JOIN #data d ON d.PathStart = p.LastCave
+			WHERE p.LastCave <> 'end' -- Only add to paths without an end
+				AND IIF(CHARINDEX(','+d.PathEnd+',', p.FullPath) > 0 AND d.PathEnd = LOWER(d.PathEnd), 0, 1) = 1 -- ignore adding new paths that are illegal
+		) n ON 1=0
+		WHEN NOT MATCHED BY TARGET
+		THEN INSERT (FullPath, LastCave) VALUES (n.FullPath, n.LastCave)
+		WHEN NOT MATCHED BY SOURCE AND o.LastCave <> 'end'
+		THEN DELETE;
+
+		IF (@@ROWCOUNT = 0) BREAK;
+	END;
+
+	SELECT Answer = COUNT(*)
+	FROM #paths p
+------------------------------------------------------------------------------
+GO
+------------------------------------------------------------------------------
+-- Part 2
+------------------------------------------------------------------------------
+	/* Part 2 is exactly the same as part 1, but the logic to identify legal paths has changed
+	   Now, instead of allowing a small cave to appear in the path once, we need to allow it to appear twice, but only for 1 cave.
+
+	   I think the easiest way to do this would be to use a cross apply function that splits the path using STRING_SPLIT
+	   Then it filters by small caves, and takes a group by count...GROUP BY cave HAVING COUNT(*) > 1
+	*/
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
